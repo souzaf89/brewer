235bb19b6578e608d9f4e55395f5003f958e8bee
Fagner Souza <fagner.roberto89@gmail.com>
2021-01-13 06:46:52 -0300

    Validando a descrição

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,5 +1,7 @@
 package com.algaworks.brewer.model;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class Cerveja {
@@ -7,8 +9,12 @@ public class Cerveja {
 	@NotBlank
 	private String sku;
 	
+	@NotBlank
 	private String nome;
-		
+	
+	@Size(min = 1, max = 50)
+	private String descricao;
+			
 	public String getSku() {		
 		return sku;
 	}
@@ -21,6 +27,11 @@ public class Cerveja {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 	
 }
